0987ff164f267acb7f54231966f08b5956d2bff2
Sima <sima.boreiri@gmail.com>
2020-08-05 16:16:17 +0200

    setup the rspec as test environment configure factory bot and shoulda matcher in rails helper, clean up rails helper and spec helper, creayes new directory as support and adds shoulda matcher file and config that

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require "rails"
+
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -14,14 +15,16 @@ require "action_cable/engine"
 
 Bundler.require(*Rails.groups)
 
-module CooperApi2
+module CooperApi
   class Application < Rails::Application
+    
     config.load_defaults 6.0
+    config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*'
-          headers: :any,
+        resource '*', 
+          headers: :any, 
           methods: %i[get post put delete],
           expose: %w(access-token expiry token-type uid client),
           max_age: 0
